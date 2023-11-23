3e82bd0f3921553df3bfdd32f1b1e17d6bb1ee57
XSheldon <marie.brayer@gmail.com>
2023-11-23 10:32:13 +0000

    fixed about

--- src/utils/bin/about.ts
+++ src/utils/bin/about.ts
@@ -2,7 +2,7 @@ import { getBio } from '../../api';
 
 export const about = async (args: string[]): Promise<string> => {
   //const bio = await getBio();
-  const tldr = "We are launch the first Pan European virtual conference dedicated to the state of the art of AI x SWE. Our first edition will take place on the 26/01 at 12AM UK. Format: 3 speakers, each presenting a technical achievement / breakthrough in a 15-20min format + Q&A. Our speakers are *Haarm de Vries*, VP of AI at Servicenow and lead of the Bigcode open source project, who will present their new model, StarCoder 2, *Jamie Shotton* CSO of Wayve who will talk about the performance inflexion point their AV model is experiencing with the data scale, and finally *Léo Dreyfus-Schmidt* VP Research at Dataiku who will share his insights on as the the state of the art of LLM deployment in the enterprise.To sign up, you need to find the password!"
-"
+  const tldr = "We are launch the first Pan European virtual conference dedicated to the state of the art of AI x SWE. Our first edition will take place on the 26/01 at 12AM UK. Format: 3 speakers, each presenting a technical achievement / breakthrough in a 15-20min format + Q&A. Our speakers are *Haarm de Vries*, VP of AI at Servicenow and lead of the Bigcode open source project, who will present their new model, StarCoder 2, *Jamie Shotton* CSO of Wayve who will talk about the performance inflexion point their AV model is experiencing with the data scale, and finally *Léo Dreyfus-Schmidt* VP Research at Dataiku who will share his insights on as the the state of the art of LLM deployment in the enterprise.To sign up, you need to find the password!";
+
   return tldr;
 };
